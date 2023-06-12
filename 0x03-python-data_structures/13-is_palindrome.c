070265d1562a04b624686d1b2c1a19bc408861bd
Heba Omar <jtvonline@yahoo.com>
2023-06-12 22:22:36 +0300

    a function in C that checks if a singly linked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,21 +3,38 @@
 #include <string.h>
 #include "lists.h"
 
+/**
+ * reverse_string - reverses a string in place
+ * @s: string to reverse
+ */
+void reverse_string(char *s)
+{
+int len = strlen(s);
+int i;
+char temp;
+
+for (i = 0; i < len / 2; i++)
+{
+temp = s[i];
+s[i] = s[len - i - 1];
+s[len - i - 1] = temp;
+}
+}
 
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: head of the linked list
  *
- * Return: 0 if it is a palindrome and 1 if otherwise
+ * Return: 1 if it is a palindrome and 0 if otherwise
  */
-
 
 int is_palindrome(listint_t **head)
 {
-
-char str[] = malloc(sizeof(char) * 1000);
+char *str;
 int i = 0;
 listint_t *node = *head;
+
+str = malloc(sizeof(char) * 1000);
 
 while (node)
 {
@@ -25,11 +42,23 @@ str[i] = (char)node->n;
 i++;
 node = node->next;
 }
+str[i] = '\0';
 
-if (strcmp(str, strrev(str)) == 0)
+reverse_string(str);
+
+node = *head;
+i = 0;
+while (node)
 {
+if (str[i] != (char)node->n)
+{
+free(str);
 return (0);
 }
+i++;
+node = node->next;
+}
+
 free(str);
 return (1);
 }
