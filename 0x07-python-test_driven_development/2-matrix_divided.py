440478a4cf9ef7d700195acb85425ebbd0fdb014
Heba Omar <jtvonline@yahoo.com>
2023-07-07 22:01:04 +0300

    2. Say my name

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 """ Integers Division """
 
 
@@ -16,23 +14,25 @@ def matrix_divided(matrix, div):
         Raises:
         TypeError: If either a or b is not an int or float.
     """
+
     if type(div) is not int and type(div) is not float:
-        raise TypeError("div must be a number")
+        raise TypeError('div must be a number')
     if div == 0:
-        raise ZeroDivisionError("division by zero")
-    if not matrix or not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise ZeroDivisionError('division by zero')
+    if not matrix or not isinstance(matrix, list) \
+            or not all(isinstance(row, list) for row in matrix):
+        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
 
     for rows in matrix:
         if type(rows) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
         for i in rows:
             if type(i) is not int and type(i) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
     list_len = len(matrix[0])
     for submatrix in matrix:
         if len(submatrix) != list_len:
-            raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError('Each row of the matrix must have the same size')
 
     new_list = []
     for row in matrix:
