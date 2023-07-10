38136ac889e248fabe37fc6dd49ef2d0cf5525d4
Heba Omar <jtvonline@yahoo.com>
2023-07-10 21:43:01 +0300

    a class MyList that inherits from list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,19 +5,5 @@
 
 class MyList(list):
     """Define MyList class"""
-
-
-    def __init__(self, mylist):
-        """Initialize constructor"""
-        self.__mylist = mylist
-
-    @property
-    def mylist(self):
-        """Get/set the list"""
-        return self.__mylist
-    @mylist.setter
-    def mylist(self, value):
-        self.__mylist = value
-
     def print_sorted(self):
-        self.sort()
+        print(sorted(self))
