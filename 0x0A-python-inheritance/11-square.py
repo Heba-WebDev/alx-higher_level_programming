1b81360963f9f23193c25c7ee8d0741494030c89
Heba Omar <jtvonline@yahoo.com>
2023-07-11 02:37:00 +0300

    a class Square that inherits from Rectangle

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
 """
-This program use the inherit for create a new Rectangle
+This program improves a previous Square
 """
 
 
-prevRectangle = __import__('9-rectangle').Rectangle
+PrevSquare = __import__('10-square').Square
 
 
-class Square(prevRectangle):
+class Square(PrevSquare):
     """
-    Class Square inherits from Rectangle class
+    Improved Class Square
     """
 
     def __init__(self, size):
-        """Constructor of new Square"""
-        self.integer_validator("size", size)
-        self.__size = size
-        super().__init__(size, size)
-
-    def area(self):
-        """This method return the area of the Square"""
-        return self.__size ** 2
+        """Constructor of the improved Square"""
+        super().__init__(size)
 
     def __str__(self):
-        """This method return the representation of the Square"""
-        return '[Square] {:d}/{:d}'.format(self.__width, self.__height)
+        """String representation of Square"""
+        return '[Square] {0:d}/{0:d}'.format(self.__size)
