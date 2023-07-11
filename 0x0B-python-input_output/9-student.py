eb54ab3257ce5194d213bb4bc497a1a339e7dbe7
Heba Omar <jtvonline@yahoo.com>
2023-07-11 21:47:09 +0300

    a class Student that defines a student by first name, last name and age

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,10 +5,18 @@
 
 class Student():
     """Define Student class"""
+
     def __init__(self, first_name, last_name, age):
+        """Initialize a new Student object
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.firstname = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        return (self.__dict__)
+        """Get a dict representation of Student"""
+        return self.__dict__
