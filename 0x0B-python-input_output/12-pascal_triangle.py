3a53553e85ad68fd0cf45f4c911cf665e8ea0282
Heba Omar <jtvonline@yahoo.com>
2023-07-11 22:58:21 +0300

    Technical interview preparation:  function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -12,11 +12,11 @@ def pascal_triangle(n):
     for i in range(n):
         if i == 0:
             my_list.append([1])
-        new_list = [] * i+1
-        for j in new_list:
-            if j == 0:
-                new_list.append(my_list[i][j])
-            if j == len(new_list) - 1:
-                new_list.append(my_list[i][j])
-            new_list.append(my_list[i][j-1] + my_list[i][j])
+        else:
+            new_list = [0] * (i+1)
+        for j in range(len(new_list)):
+            if j == 0 or j == len(new_list) - 1:
+                new_list.append(1)
+            new_list[j] = my_list[i][j-1] + my_list[i][j]
+
     return (my_list)
