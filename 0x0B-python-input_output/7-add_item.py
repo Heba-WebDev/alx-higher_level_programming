3b61a6217996d60c7bda3e43f4c3b788a94272f5
Heba Omar <jtvonline@yahoo.com>
2023-07-11 21:32:49 +0300

    script that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
 """Creates an Object from a “JSON file”"""
+import sys
 
-import argparse
-load = __import__('6-load_from_json_file').load_from_json_file
-save = __import__('5-save_to_json_file').save_to_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-my_list = []
-parser = argparse.ArgumentParser(description='parse arguments')
-parser.add_argument('args', metavar='N', type=str, nargs='+',
-                    help='an integer for the accumulator')
-args = parser.parse_args()
-
-for arg in args:
-    my_list.append(arg)
-
-try:
-    my_list = load('add_item.json')
-except FileNotFoundError:
-    pass
-
-save(my_list + args.args, 'add_item.json')
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        list = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
