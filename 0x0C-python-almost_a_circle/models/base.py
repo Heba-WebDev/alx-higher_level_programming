ee1c7a0a4706e6b590fc6db1d927f9590080f05b
Heba Omar <jtvonline@yahoo.com>
2023-07-13 21:18:06 +0300

    1. Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,11 +4,11 @@
 
 class Base:
     """Class initilization"""
+    __nb_objects = 0
 
     def __init__(self, id=None):
-        """Instances of base"""
-        self.__nb_objects = 0
-        if id is not None:
-            self.id = id
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
         else:
-            self.__nb_objects += 1
+            self.id = id
