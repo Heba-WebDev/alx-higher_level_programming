61880d1845343f200b06de5f155103ba2c50f9ca
Heba Omar <jtvonline@yahoo.com>
2023-09-12 21:56:50 +0300

    12. Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const dict = require('./101-data.js').dict;
+const userIdsByOccurrence = {};
+
+for (const userId in dict) {
+  const occurrence = dict[userId];
+
+  if (!userIdsByOccurrence[occurrence]) {
+    userIdsByOccurrence[occurrence] = [userId];
+  } else {
+    userIdsByOccurrence[occurrence].push(userId);
+  }
+}
+
+console.log(userIdsByOccurrence);
