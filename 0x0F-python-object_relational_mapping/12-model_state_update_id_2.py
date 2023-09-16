0ca8a34070b58e2db21c8eae8044e4c19e0603b3
Heba Omar <jtvonline@yahoo.com>
2023-09-16 20:07:51 +0300

    Write a script that changes the name of a State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -23,4 +23,5 @@ if __name__ == "__main__":
     session = Session()
     state_to_update = session.query(State).filter_by(id='2').first()
     state_to_update.name = "New Mexico"
+    session.commit()
     session.close()
