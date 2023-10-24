e7e776d01314ccd5dfaa3a9d8f45e32e233a3210
Heba Omar <jtvonline@yahoo.com>
2023-10-24 20:31:04 +0300

    Write a script that prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,24 @@
 #!/usr/bin/node
 
-const request = require('request-promise-native');
-const movieId = process.argv[2];
+const request = require('request');
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  }
-  const characters = JSON.parse(body).characters;
-  printCharacters(characters, 0);
-});
-
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
     if (error) {
-      console.log(error);
+        console.log(error);
     }
-    console.log(JSON.parse(body).name);
-    if (index + 1 < characters.length) {
-      printCharacters(characters, index + 1);
-    }
+      const characters = JSON.parse(body).characters;
+      printCharacters(characters, 0);
   });
-}
+
+  function printCharacters (characters, index) {
+    request(characters[index], function (error, response, body) {
+      if (error) {
+        console.log(error);
+      }
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+        }
+    });
+  }
